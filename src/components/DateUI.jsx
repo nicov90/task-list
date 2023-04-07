95ba9ac9ce531a1aecc2effc3bdea4b8d2ada4cf
Nicolás Valdez <nico_v99@hotmail.com>
2023-04-07 18:14:18 -0300

    Changed date format of tasks

--- src/components/DateUI.jsx
+++ src/components/DateUI.jsx
@@ -1,48 +1,51 @@
-import React, { useContext } from 'react';
-import { useState, useEffect } from 'react';
-import moment from 'moment';
-import '../css/Date.css';
-import { GlobalContext } from '../App';
+import React, { useContext } from "react";
+import { useState, useEffect } from "react";
+import moment from "moment";
+import "../css/Date.css";
+import { GlobalContext } from "../App";
 
 function DateUI({ id, taskDate }) {
-    const { currentDate } = useContext(GlobalContext);
-    const [date, setDate] = useState("");
-    useEffect(()=>{
-        try{
-            if(id === 'currentDate'){
+  const { currentDate } = useContext(GlobalContext);
+  const [date, setDate] = useState("");
+  useEffect(() => {
+    try {
+      if (id === "currentDate") {
+        const updateDate = () => {
+          let currentTitleDate = moment().format("MMMM, DD dddd");
+          setDate(currentTitleDate);
+        };
+        updateDate();
 
-                const updateDate = ()=>{
-                    let currentTitleDate = moment().format("MMMM, DD dddd");
-                    setDate(currentTitleDate);
-                }
-                updateDate();
+        //* Intervalo que verifica la fecha actual a todo momento
+        //* y lo mantiene actualizado.
+        const intervalCurrentDate = setInterval(updateDate, 1000);
 
-                //* Intervalo que verifica la fecha actual a todo momento
-                //* y lo mantiene actualizado.
-                const intervalCurrentDate = setInterval(updateDate,1000);
-    
-                //* Se detiene el intervalo si el componente se desmonta
-                return () => clearInterval(intervalCurrentDate);
-
-            }else if(id === 'task-date'){
-                if(taskDate === currentDate){
-                    setDate("Today");
-                }else{
-                    const toBeFormattedDate = taskDate.split('-').reverse();
-                //* Remueve el año
-                toBeFormattedDate.pop();
-                const formattedDate = toBeFormattedDate.join('-');
-
-                setDate(formattedDate);
-                }
-            }
-        }catch(error){
-            console.error(error);
+        //* Se detiene el intervalo si el componente se desmonta
+        return () => clearInterval(intervalCurrentDate);
+      } else if (id === "task-date") {
+        if (taskDate === currentDate) {
+          setDate("Today");
+        } else {
+          const dateOptions = {
+            month: "short",
+            day: "2-digit",
+          };
+          const formattedDate = new Date(taskDate).toLocaleDateString(
+            "en-US",
+            dateOptions
+          );
+          setDate(formattedDate);
         }
-    }, [id, taskDate, currentDate]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, [id, taskDate, currentDate]);
 
-    return(
-        <p className='date' id={ id }>{date}</p>
-    )
+  return (
+    <p className="date" id={id}>
+      {date}
+    </p>
+  );
 }
 export default DateUI;
